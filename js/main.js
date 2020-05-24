8509d9816792ffbfe878e2c1bc6c7dc1c8b10887
KelvinHere <kwardeu@gmail.com>
2020-05-24 17:42:35 +0000

    json to table

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-function getData(cb){
+const baseURL = 'https://ci-swapi.herokuapp.com/api/';
+
+//Get data from web
+function getData(type, cb){
     let xhr = new XMLHttpRequest(); //creates XML object
     let data;
 
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //point xhr to data (get gets / post sends)
+    xhr.open("GET", baseURL + type + '/'); //point xhr to data (get gets / post sends)
     xhr.send(); //send request for that data
 
     //XHR states 0=unsent 1=opened 2=headers_received 3=loading 4=done
@@ -14,14 +17,38 @@ function getData(cb){
     };
 }
 
-function printDataToConsole(data) {
-    console.log(data);
+
+function getTableHeaders(obj) {
+    let tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
-getData(printDataToConsole);
+//place data in div
+function writeToDocument(type) {
+    let tableRows = [];
+    let el = document.getElementById("data"); //variable for div
+    el.innerHTML = ""; //empty div
 
-/*setTimeout fires its attached function when the time at the end is up
-setTimeout(function() {
-    //500ms should be enought time for data to fill up
-    console.log(data); //End point for the data we want to be able to access it outside the above functions
-}, 500)*/
+    getData(type, function(data) {  //grab info from json
+        data = data.results;
+        let tableHeaders = getTableHeaders(data[0]);  //send firt array element (all headers)
+            
+        data.forEach(function(item) {
+            let dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                let rowData = item[key].toString();
+                let truncatedData = rowData.substring(0,15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+
+        });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+    });
+}
